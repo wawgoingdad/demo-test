0781f7ee76b44041c8d9a71121c837cbfa2ecbc1
Huy Mai <huy.mai@insightus.com.au>
2024-05-24 06:16:46 +0700

    chore: update animation for text welcome

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,7 +18,7 @@ const LoginPage: React.FC = () => {
     const dispatch = useAppDispatch();
     const [formData, setFormData] = useState({ email: "", password: "" });
     const [errors, setErrors] = useState<FormErrors>({});
-    const { loading } = useAuthentication();
+    const { loading, isAuthenticated } = useAuthentication();
 
     const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
@@ -34,6 +34,13 @@ const LoginPage: React.FC = () => {
         try {
             userSchema.parse(formData);
             dispatch(login(formData as User));
+            // if (result.type.endsWith("fulfilled")) {
+                // setWelcomeMessage(`Welcome back, ${formData.email}!`);
+                // setSuccess(true);
+                // setTimeout(() => {
+                //     setSuccess(false);  // Resets the login form after a delay
+                // }, 3000);
+            // }
         } catch (error) {
             if (error instanceof z.ZodError) {
                 const newErrors: FormErrors = {};
@@ -61,104 +68,124 @@ const LoginPage: React.FC = () => {
                         </div>
                     ) : null}
 
-                    <div className="col-xl-4 col-lg-4 col-md-6 col-sm-7 justify-content-center align-items-center login-card">
-                        <div className="card mt-5">
-                            <div className="card-body">
-                                <div className="nav nav-tabs border-bottom-0">
-                                    <div
-                                        className="tab-content w-100"
-                                        id="nav-tabContent"
-                                    >
+                    {isAuthenticated ? (
+                        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-7 justify-content-center align-items-center">
+                            <div className="welcome-message">
+                                <h2>{t("welcomeBack")}</h2>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className={`col-xl-4 col-lg-4 col-md-6 col-sm-7 justify-content-center align-items-center login-card`}>
+                            <div className="card mt-5">
+                                <div className="card-body">
+                                    <div className="nav nav-tabs border-bottom-0">
                                         <div
-                                            className="tab-pane fade active show"
-                                            id="nav-personal"
+                                            className="tab-content w-100"
+                                            id="nav-tabContent"
                                         >
-                                            <form
-                                                className="dz-form pb-3"
-                                                onSubmit={handleSubmit}
+                                            <div
+                                                className="tab-pane fade active show"
+                                                id="nav-personal"
                                             >
-                                                <div className="mb-5">
-                                                    <h3 className="form-title m-t0">
-                                                        {t("welcomeBack")}
-                                                    </h3>
-                                                    <div className="dz-separator-outer m-b5">
-                                                        <div className="dz-separator bg-primary style-liner"></div>
+                                                <form
+                                                    className="dz-form pb-3"
+                                                    onSubmit={handleSubmit}
+                                                >
+                                                    <div className="mb-5">
+                                                        <h3 className="form-title m-t0">
+                                                            {t("welcomeBack")}
+                                                        </h3>
+                                                        <div className="dz-separator-outer m-b5">
+                                                            <div className="dz-separator bg-primary style-liner"></div>
+                                                        </div>
+                                                        <p>
+                                                            {t(
+                                                                "excitedToSeeYou"
+                                                            )}
+                                                        </p>
                                                     </div>
-                                                    <p>
-                                                        {t("excitedToSeeYou")}
-                                                    </p>
-                                                </div>
-                                                <div className="form-group mb-3">
-                                                    <label className="form-label">
-                                                        {t("email")}
-                                                    </label>
-                                                    <input
-                                                        type="text"
-                                                        className="form-control"
-                                                        name="email"
-                                                        value={formData.email}
-                                                        onChange={handleChange}
-                                                    />
-                                                    {errors.email && (
-                                                        <div className="error text-danger fs-12">
-                                                            {errors.email}
-                                                        </div>
-                                                    )}
-                                                </div>
-                                                <div className="form-group mb-3">
-                                                    <label className="form-label">
-                                                        {t("password")}
-                                                    </label>
-                                                    <input
-                                                        type="password"
-                                                        className="form-control"
-                                                        name="password"
-                                                        value={
-                                                            formData.password
-                                                        }
-                                                        onChange={handleChange}
-                                                    />
-                                                    {errors.password && (
-                                                        <div className="error text-danger fs-12">
-                                                            {errors.password}
-                                                        </div>
-                                                    )}
-                                                </div>
-                                                <div className="form-group mb-3 mt-4">
-                                                    <div className="d-grip gap-2">
-                                                        <button
-                                                            type="button"
-                                                            className="btn btn-success btn-block"
-                                                            onClick={
-                                                                handleSubmit
+                                                    <div className="form-group mb-3">
+                                                        <label className="form-label">
+                                                            {t("email")}
+                                                        </label>
+                                                        <input
+                                                            type="text"
+                                                            className="form-control"
+                                                            name="email"
+                                                            value={
+                                                                formData.email
                                                             }
-                                                        >
-                                                            {t("login")}
-                                                        </button>
+                                                            onChange={
+                                                                handleChange
+                                                            }
+                                                        />
+                                                        {errors.email && (
+                                                            <div className="error text-danger fs-12">
+                                                                {errors.email}
+                                                            </div>
+                                                        )}
                                                     </div>
-                                                </div>
-                                                <div className="form-group mb-5 mt-1 d-flex justify-content-center">
-                                                    {/* <button
+                                                    <div className="form-group mb-3">
+                                                        <label className="form-label">
+                                                            {t("password")}
+                                                        </label>
+                                                        <input
+                                                            type="password"
+                                                            className="form-control"
+                                                            name="password"
+                                                            value={
+                                                                formData.password
+                                                            }
+                                                            onChange={
+                                                                handleChange
+                                                            }
+                                                        />
+                                                        {errors.password && (
+                                                            <div className="error text-danger fs-12">
+                                                                {
+                                                                    errors.password
+                                                                }
+                                                            </div>
+                                                        )}
+                                                    </div>
+                                                    <div className="form-group mb-3 mt-4">
+                                                        <div className="d-grip gap-2">
+                                                            <button
+                                                                type="button"
+                                                                className="btn btn-success btn-block"
+                                                                onClick={
+                                                                    handleSubmit
+                                                                }
+                                                            >
+                                                                {t("login")}
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                    <div className="form-group mb-5 mt-1 d-flex justify-content-center">
+                                                        {/* <button
                                                         type="button"
                                                         className="btn btn-link text-center"
                                                     >
                                                         {t("forgetPassword")}
                                                     </button> */}
-                                                    <Link
-                                                        to="/forget-password"
-                                                        className="btn btn-link text-center"
-                                                    >
-                                                        {t("forgetPassword")}
-                                                    </Link>
-                                                </div>
-                                            </form>
+                                                        <Link
+                                                            to="/forget-password"
+                                                            className="btn btn-link text-center"
+                                                        >
+                                                            {t(
+                                                                "forgetPassword"
+                                                            )}
+                                                        </Link>
+                                                    </div>
+                                                </form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                                <div className="card-footer mt-5"></div>
                             </div>
-                            <div className="card-footer mt-5"></div>
                         </div>
-                    </div>
+                    )}
                 </div>
             </div>
         </React.Fragment>
